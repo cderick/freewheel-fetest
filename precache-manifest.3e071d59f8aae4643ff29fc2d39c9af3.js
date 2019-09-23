64dc8f3c169c23be6ff03de813ea45fdcb22a5dc
Cristian <communication-world@live.com>
2019-09-23 22:31:50 +0100

    Adding build files to emulate the project :)

---
+++ precache-manifest.3e071d59f8aae4643ff29fc2d39c9af3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c3d8e38b835909c5dda0db8378b807",
+    "url": "index.html"
+  },
+  {
+    "revision": "a0dee5c436e228ba9e05",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4feeff1f9db4b57990fa",
+    "url": "./static/css/main.695d4d7f.chunk.css"
+  },
+  {
+    "revision": "a0dee5c436e228ba9e05",
+    "url": "./static/js/2.53e30a43.chunk.js"
+  },
+  {
+    "revision": "4feeff1f9db4b57990fa",
+    "url": "./static/js/main.ebd116aa.chunk.js"
+  },
+  {
+    "revision": "44d91e0c68b5610468e4",
+    "url": "./static/js/runtime-main.1b7807d0.js"
+  },
+  {
+    "revision": "15fa3062f8929bd3b05fdca5259db412",
+    "url": "./static/media/Roboto-Regular.15fa3062.woff2"
+  },
+  {
+    "revision": "77099da87689a8fb6316b3bc050fe3fc",
+    "url": "./static/media/cyclebanner.77099da8.png"
+  },
+  {
+    "revision": "bb01ab84dfdb6aeceb578d37f5d388eb",
+    "url": "./static/media/loadingImage.bb01ab84.gif"
+  },
+  {
+    "revision": "9f024cc974d36a861a9e898f0b3610ca",
+    "url": "./static/media/underground-banner.9f024cc9.png"
+  }
+]);
