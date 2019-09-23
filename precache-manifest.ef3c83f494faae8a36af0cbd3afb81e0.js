bf9fd9de8602e8c72409afc517d18a1786818ca7
Cristian <communication-world@live.com>
2019-09-23 22:38:46 +0100

    Fixing relative paths :)

---
+++ precache-manifest.ef3c83f494faae8a36af0cbd3afb81e0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95295f94746e0edd38511cf5f5aadf86",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0dee5c436e228ba9e05",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ddad18afc44cdcc3374a",
+    "url": "./static/css/main.468c841d.chunk.css"
+  },
+  {
+    "revision": "a0dee5c436e228ba9e05",
+    "url": "./static/js/2.53e30a43.chunk.js"
+  },
+  {
+    "revision": "ddad18afc44cdcc3374a",
+    "url": "./static/js/main.0d84244f.chunk.js"
+  },
+  {
+    "revision": "33dad8a42d1ebc005929",
+    "url": "./static/js/runtime-main.d0608a9c.js"
+  },
+  {
+    "revision": "15fa3062f8929bd3b05fdca5259db412",
+    "url": "./static/media/Roboto-Regular.15fa3062.woff2"
+  },
+  {
+    "revision": "77099da87689a8fb6316b3bc050fe3fc",
+    "url": "./static/media/cyclebanner.77099da8.png"
+  },
+  {
+    "revision": "bb01ab84dfdb6aeceb578d37f5d388eb",
+    "url": "./static/media/loadingImage.bb01ab84.gif"
+  },
+  {
+    "revision": "9f024cc974d36a861a9e898f0b3610ca",
+    "url": "./static/media/underground-banner.9f024cc9.png"
+  }
+]);
